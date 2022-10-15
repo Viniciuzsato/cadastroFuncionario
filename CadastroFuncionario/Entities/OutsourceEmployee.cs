26adbed0cbfcbf24b3d1313b2dadd7c26322e35e
vinicius sato <vinicius.sato@outlook.com>
2022-10-15 17:17:09 -0300

    commit initial

---
+++ CadastroFuncionario/Entities/OutsourceEmployee.cs
@@ -0,0 +1,24 @@
+﻿
+namespace cadastroFuncionario.Entities
+{
+    internal class OutsourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourceEmployee()
+        {
+
+        }
+
+        public OutsourceEmployee(string name, int hours, double valuePerHours, double additionalCharge)
+            : base(name, hours, valuePerHours)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
---
+++ CadastroFuncionario/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using cadastroFuncionario.Entities;
+
+namespace cadastroFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Entre com número de funcionários: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Dados do funcionário {i}: ");
+                Console.Write("Tercerizado (s/n)?");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Horas: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Valor por Hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(ch == 's')
+                {
+                    Console.Write("Despesa Adicional: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pagamento:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
